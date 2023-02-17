6ada2699f456b29424f809cd7f60bcc2f84a48e9
Nagendra2140 <94901314+Nagendra2140@users.noreply.github.com>
2023-02-17 09:48:58 +0530

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd /var/www/html/node-js-sample/
+cd /var/www/html/
 
 sudo npm install
 
 #To execute this command you need to create service for nodejs application in remote server
 
-sudo systemctl restart node.service
+#sudo systemctl restart node.service
 
 #If your are using PM2. use below commands(un-comment)
 
-#pm2 start cms.js
+pm2 start cms.js
 
 #pm2 restart cms
