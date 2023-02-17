83a15bb930bfa52bcd6e07c084d0d972fe33c009
Nagendra2140 <94901314+Nagendra2140@users.noreply.github.com>
2023-02-17 13:55:03 +0530

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /var/www/node_cicd/hello/
+
+zip -r node-`date +%Y%m%d_%H%M%S`.zip /var/www/node_cicd/hello/* && mv *.zip /bkp/
+
+git pull origin master
+
+sudo npm install
+
+#To execute this command you need to create service for nodejs application in remote server
+
+#sudo systemctl restart node.service
+
+#If your are using PM2. use below commands(un-comment)
+
+#pm2 start cms.js
+
+pm2 -f restart cms
