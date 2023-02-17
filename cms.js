c10a1b3a859d1e8781938f723a565f5f7cb2225c
Nagendra2140 <94901314+Nagendra2140@users.noreply.github.com>
2023-02-17 14:10:35 +0530

    Update cms.js

--- cms.js
+++ cms.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 6000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Nagendra master-- new changes---buhgtyftyrx')
+  response.send('Hello Nagendra master')
 })
 
 app.listen(app.get('port'), function() {
