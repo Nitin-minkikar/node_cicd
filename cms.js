955e6a487e94c87df95b985caabba6f6989dd2ef
Nagendra2140 <94901314+Nagendra2140@users.noreply.github.com>
2023-02-17 17:35:34 +0530

    Update cms.js

--- cms.js
+++ cms.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 7000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Nagendra master--new changes')
+  response.send('Hello Nagendra master--new changes--pandago')
 })
 
 app.listen(app.get('port'), function() {
