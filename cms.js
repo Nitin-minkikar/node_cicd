bc047e8f2c803f0e9ca33a0b57bbf12894f6395d
Nitin-minkikar <nk10081@gmail.com>
2025-04-08 15:20:35 +0530

    Update cms.js

--- cms.js
+++ cms.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 7000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Nitin Prod it is v1.5')
+  response.send('Hello Nitin minkikar Prod')
 });
 
 app.listen(app.get('port'), function() {
